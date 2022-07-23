66ee1b402187f283980179074fc4496c35c8d8ab
Gatofeles <alexofeles@gmail.com>
2022-07-23 12:54:24 -0300

    setting up

---
+++ expense/src/components/ExpenseTable/ExpenseTable.jsx
@@ -0,0 +1,12 @@
+import ExpenseItem from '../expenseitem/ExpenseItem.jsx';
+import './ExpenseTable.css';
+
+const ExpenseTable = (props) => {
+    return (
+        <div className="expense-table">
+            {props.expense.map((e) => <ExpenseItem key={e.id} expense={e} />)}
+        </div>
+    );
+}
+
+export default ExpenseTable;
